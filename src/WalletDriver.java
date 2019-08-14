5ad08a63ed1cbf29fc05e4f539f374201737f067
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-08-14 13:45:55 -0400

    started working on unit tests

---
+++ src/WalletDriver.java
@@ -0,0 +1,21 @@
+public class WalletDriver {
+
+    private Wallet wallet;
+
+    private void beforeEachTest() {
+
+    } // beforeEachTest
+
+    private void afterEachTest() {
+
+    } // afterEachTest
+
+    /**
+     * Entry point into the application.
+     * @param args  the command-line arguments
+     */
+    public static void main(String[] args) {
+
+    } // main
+
+} // WalletDriver
